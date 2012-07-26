93deb1c0157fe90ea7b23364ecd654bc15441ed9
renstrom <renstrom.peter@gmail.com>
2012-07-26 22:45:04 +0200

    Priest: Add some basic Shadow spells.

--- classes/priest.lua
+++ classes/priest.lua
@@ -4,9 +4,9 @@ local _, addon = ...
 local playerName, _ = UnitName('player')
 
 addon:Debuff({
-	spellID = 172, -- Corruption
+	spellID = 34914, -- Vampiric Touch
 	size = 36,
-	position = {'CENTER', 'UIParent', 'CENTER', -150, 0},
+	position = {'CENTER', 'UIParent', 'CENTER', -210, 0},
 	unit = 'target',
 	validateUnit = true,
 	hideOutOfCombat = true,
@@ -16,20 +16,34 @@ addon:Debuff({
 })
 
 addon:Debuff({
-	spellID = 980, -- Agony
-	position = {'CENTER', 'UIParent', 'CENTER', -150, -42},
+	spellID = 589, -- Shadow Word: Pain
+	size = 36,
+	position = {'CENTER', 'UIParent', 'CENTER', -210, -42},
 	unit = 'target',
 	validateUnit = true,
 	hideOutOfCombat = true,
 	isMine = true,
+	desaturate = true,
 	movable = false,
 })
 
-addon:Buff({
-	spellID = 109773,
-	position = {'CENTER', 'UIParent', 'CENTER', 150, 0},
+addon:Debuff({
+	spellID = 2944, -- Devouring Plague
+	size = 36,
+	position = {'CENTER', 'UIParent', 'CENTER', -210, -84},
+	unit = 'target',
+	validateUnit = true,
+	hideOutOfCombat = true,
+	isMine = true,
+	desaturate = true,
+	movable = false,
 })
 
 addon:Cooldown({
-	spellID = 6229,
+	spellID = 8092, -- Mind Blast
+	size = 36,
+	position = {'CENTER', 'UIParent', 'CENTER', -252, 0},
+	hideOutOfCombat = true,
+	desaturate = true,
+	movable = false,
 })
