5540a301bd03322c800b8fe68f91d75de648fb3e
Peter Renström <renstrom.peter@gmail.com>
2012-07-27 16:25:13 +0200

    Druid: Add Rake, Rip and Savage Roar to Feral spec.

--- classes/druid.lua
+++ classes/druid.lua
@@ -5,6 +5,7 @@ local playerName, _ = UnitName('player')
 
 addon:Debuff({
 	spellID = {8921, 93402}, -- Moonfire & Sunfire
+	spec = 1,
 	size = 36,
 	position = {'CENTER', 'UIParent', 'CENTER', -210, -42},
 	unit = 'target',
@@ -15,8 +16,43 @@ addon:Debuff({
 	movable = false,
 })
 
+addon:Debuff({
+	spellID = 1822, -- Rake
+	spec = 2,
+	size = 36,
+	position = {'CENTER', 'UIParent', 'CENTER', -210, -42},
+	unit = 'target',
+	validateUnit = true,
+	hideOutOfCombat = true,
+	isMine = true,
+	desaturate = true,
+	movable = false,
+})
+
+addon:Debuff({
+	spellID = 1079, -- Rip
+	spec = 2,
+	size = 36,
+	position = {'CENTER', 'UIParent', 'CENTER', -210, 0},
+	unit = 'target',
+	validateUnit = true,
+	hideOutOfCombat = true,
+	isMine = true,
+	desaturate = true,
+	movable = false,
+})
+
+addon:Buff({
+	spellID = 52610, -- Savage Roar
+	spec = 2,
+	position = {'CENTER', 'UIParent', 'CENTER', -210, 42},
+	hideOutOfCombat = true,
+	movable = false,
+})
+
 addon:Cooldown({
 	spellID = 78674, -- Starsurge
+	spec = 1,
 	size = 36,
 	position = {'CENTER', 'UIParent', 'CENTER', -210, 0},
 	hideOutOfCombat = true,
@@ -26,6 +62,7 @@ addon:Cooldown({
 
 addon:Cooldown({
 	spellID = 48505, -- Starfall
+	spec = 1,
 	size = 36,
 	position = {'CENTER', 'UIParent', 'CENTER', -252, 0},
 	hideOutOfCombat = true,
