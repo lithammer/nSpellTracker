1e8a431aea6a6894ac5906c5096ce85232a1e17e
renstrom <renstrom.peter@gmail.com>
2012-08-04 14:35:33 +0200

    Hunter: Track Serpent Sting.

--- classes/hunter.lua
+++ classes/hunter.lua
@@ -4,32 +4,10 @@ local _, addon = ...
 local playerName, _ = UnitName('player')
 
 addon:Debuff({
-	spellID = 172, -- Corruption
-	size = 36,
-	position = {'CENTER', 'UIParent', 'CENTER', -150, 0},
+	spellID = 1978,
+	position = {'CENTER', 'UIParent', 'CENTER', -210, 0}
+	isMine = true,
 	unit = 'target',
 	validateUnit = true,
-	hideOutOfCombat = true,
-	isMine = true,
-	desaturate = true,
 	movable = false,
-})
-
-addon:Debuff({
-	spellID = 980, -- Agony
-	position = {'CENTER', 'UIParent', 'CENTER', -150, -42},
-	unit = 'target',
-	validateUnit = true,
-	hideOutOfCombat = true,
-	isMine = true,
-	movable = false,
-})
-
-addon:Buff({
-	spellID = 109773,
-	position = {'CENTER', 'UIParent', 'CENTER', 150, 0},
-})
-
-addon:Cooldown({
-	spellID = 6229,
 })
