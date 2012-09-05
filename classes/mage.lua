c8fe0f08dedd5e25378103875e6dcdcfcd668eed
Peter Renström <renstrom.peter@gmail.com>
2012-09-05 08:38:17 +0200

    Mage: More Fire debuffs. - Living Bomb. - Ignite. - Pyroblast

--- classes/mage.lua
+++ classes/mage.lua
@@ -3,3 +3,44 @@ if select(2, UnitClass('player')) ~= 'MAGE' then return end
 local _, addon = ...
 local playerName, _ = UnitName('player')
 
+addon:Debuff({
+	spellID = 44457, -- Living Bomb
+	spec = 2,
+	size = 50,
+	unit = 'target',
+	position = {'CENTER', 'UIParent', 'CENTER', 0, -250},
+	unit = 'target',
+	validateUnit = true,
+	hideOutOfCombat = true,
+	isMine = true,
+	desaturate = true,
+	movable = false,
+})
+
+addon:Debuff({
+	spellID = 12654, -- Ignite
+	spec = 2,
+	size = 36,
+	unit = 'target',
+	position = {'CENTER', 'UIParent', 'CENTER', -50, -250},
+	unit = 'target',
+	validateUnit = true,
+	hideOutOfCombat = true,
+	isMine = true,
+	desaturate = true,
+	movable = false,
+})
+
+addon:Debuff({
+	spellID = 11366, -- Pyroblast
+	spec = 2,
+	size = 36,
+	unit = 'target',
+	position = {'CENTER', 'UIParent', 'CENTER', 50, -250},
+	unit = 'target',
+	validateUnit = true,
+	hideOutOfCombat = true,
+	isMine = true,
+	desaturate = true,
+	movable = false,
+})
