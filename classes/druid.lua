3df08154960f02fb240bd6fc53588993de0b247d
Peter Renström <renstrom.peter@gmail.com>
2012-09-17 21:27:25 +0200

    druid: Add some bear stuff.

--- classes/druid.lua
+++ classes/druid.lua
@@ -55,10 +55,45 @@ addon:Debuff({
 	movable = false,
 })
 
+addon:Debuff({
+	spellID = 33745, -- Lacerate
+	spec = 3,
+	size = 36,
+	position = {'CENTER', 'UIParent', 'CENTER', -210, -42},
+	unit = 'target',
+	validateUnit = true,
+	hideOutOfCombat = true,
+	isMine = true,
+	desaturate = true,
+	movable = false,
+})
+
+addon:Debuff({
+	spellID = addon.debuffs.weakenedArmor, -- Weakened Armor
+	spec = 3,
+	size = 36,
+	position = {'CENTER', 'UIParent', 'CENTER', -210, 0},
+	unit = 'target',
+	validateUnit = true,
+	hideOutOfCombat = true,
+	isMine = true,
+	desaturate = true,
+	movable = false,
+})
+
 addon:Buff({
 	spellID = 52610, -- Savage Roar
 	spec = 2,
 	position = {'CENTER', 'UIParent', 'CENTER', -210, 42},
+	hideOutOfCombat = true,
+	movable = false,
+})
+
+addon:Buff({
+	spellID = 132402, -- Savage Defence
+	spec = 3,
+	size = 50,
+	position = {'CENTER', 'UIParent', 'CENTER', 0, -250},
 	hideOutOfCombat = true,
 	movable = false,
 })
@@ -78,6 +113,16 @@ addon:Cooldown({
 	spec = 1,
 	size = 36,
 	position = {'CENTER', 'UIParent', 'CENTER', -252, -42},
+	hideOutOfCombat = true,
+	desaturate = true,
+	movable = false,
+})
+
+addon:Cooldown({
+	spellID = 33878, -- Mangle
+	spec = 3,
+	size = 36,
+	position = {'CENTER', 'UIParent', 'CENTER', -252, 0},
 	hideOutOfCombat = true,
 	desaturate = true,
 	movable = false,
