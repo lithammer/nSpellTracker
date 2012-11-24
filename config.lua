1d382ac5c1456485c1686855e80f1f8bd425f488
Peter Renström <renstrom.peter@gmail.com>
2012-11-24 17:05:54 +0100

    Don't highlight player spells by default.

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ local _, addon = ...
 local config = addon.config
 
 -- Player spells will have a blue border
-config.highlightPlayerSpells = true
+config.highlightPlayerSpells = false
 
 -- How fast should the timer update itself
 config.updatetime = 0.1
