04b1c02c430f9a49e08552c08e3b67fd0b88e89b
Peter Renström <renstrom.peter@gmail.com>
2012-11-24 17:04:14 +0100

    Warlock: Add Dark Soul and some other stuff.

--- classes/warlock.lua
+++ classes/warlock.lua
@@ -65,7 +65,6 @@ addon:Debuff({
 addon:Debuff({
 	spellID = 172, -- Corruption
 	spec = {1, 2},
-	size = 36,
 	position = {'CENTER', 'UIParent', 'CENTER', -210, 0},
 	unit = 'target',
 	desaturate = true,
@@ -104,19 +103,27 @@ addon:Debuff({
 	},
 })
 
+--[[
 addon:Debuff({
 	spellID = addon.debuffs.magicVulnerability,
 	position = {'BOTTOM', 'UIParent', 'BOTTOM', 0, 210},
+	movable = false,
 	unit = 'target',
 	isMine = false,
 })
-
---[[
-addon:Buff({
-	spellID = addon.buffs.spellPower,
-	position = {'CENTER', 'UIParent', 'CENTER', 210, 0},
-})
 ]]--
+
+addon:Buff({
+	spellID = {113858, 113860, 113861}, -- Dark Soul
+	position = {'CENTER', 'UIParent', 'CENTER', 150, 42},
+	movable = false,
+	alpha = {
+		notFound = {
+			frame = 0,
+			icon = 0,
+		}
+	},
+})
 
 addon:Buff({
 	spellID = 117896, -- Backdraft
@@ -126,7 +133,7 @@ addon:Buff({
 })
 
 addon:Buff({
-	spellID = 111400,
+	spellID = 111400, -- Burning Rush
 	position = {'CENTER', 'UIParent', 'CENTER', 100, -42},
 	hideOutOfCombat = false,
 	movable = false,
@@ -137,6 +144,80 @@ addon:Buff({
 		}
 	},
 })
+
+addon:Cooldown({
+	spellID = 113860, -- Dark Soul: Misery
+	position = {'CENTER', 'UIParent', 'CENTER', -294, 42},
+	spec = 1,
+	movable = false,
+	matchSpellID = false,
+	alpha = {
+		cooldown = {
+			frame = 0.8,
+			icon = 0.4,
+		},
+		notCooldown = {
+			frame = 0.8,
+			icon = 0.8,
+		}
+	}
+})
+
+addon:Cooldown({
+	spellID = 113858, -- Dark Soul: Instability
+	position = {'CENTER', 'UIParent', 'CENTER', -294, 42},
+	spec = 3,
+	movable = false,
+	matchSpellID = false,
+	alpha = {
+		cooldown = {
+			frame = 0.8,
+			icon = 0.4,
+		},
+		notCooldown = {
+			frame = 0.8,
+			icon = 0.8,
+		}
+	}
+})
+
+addon:Cooldown({
+	spellID = 113861, -- Dark Soul: Knowledge
+	position = {'CENTER', 'UIParent', 'CENTER', -294, 42},
+	spec = 2,
+	movable = false,
+	matchSpellID = false,
+	alpha = {
+		cooldown = {
+			frame = 0.8,
+			icon = 0.4,
+		},
+		notCooldown = {
+			frame = 0.8,
+			icon = 0.8,
+		}
+	}
+})
+
+local _, _, _, _, selected = GetTalentInfo(14)
+if selected then
+	addon:Cooldown({
+		spellID = 111898, -- Grimoire: Felguard
+		spec = 2,
+		position = {'CENTER', 'UIParent', 'CENTER', -294, 0},
+		movable = false,
+		alpha = {
+			cooldown = {
+				frame = 0.8,
+				icon = 0.4,
+			},
+			notCooldown = {
+				frame = 0.8,
+				icon = 0.8,
+			}
+		}
+	})
+end
 
 addon:Cooldown({
 	spellID = 17962, -- Conflagrate
@@ -150,4 +231,29 @@ addon:Cooldown({
 	spec = 2,
 	position = {'CENTER', 'UIParent', 'CENTER', -210, 42},
 	movable = false,
+	PostUpdateHook = function(iconframe, duration)
+		local charges, _, timeLastCast = GetSpellCharges(105174)
+		local name, _, _, count, _, _, expires, caster = UnitAura('target', 'Shadowflame', nil, 'HARMFUL')
+
+		if charges == 1 and GetTime() - timeLastCast < 9 then
+			iconframe.border:SetVertexColor(1, 0, 0)
+		end
+
+		if charges == 1 and caster then
+			iconframe.border:SetVertexColor(0.4, 0.6, 0.2)
+		end
+	end
+})
+
+addon:Cooldown({
+	spellID = 119914, -- Felstorm
+	spec = 2,
+	position = {'CENTER', 'UIParent', 'CENTER', -210, 84},
+	movable = false,
+	alpha = {
+		cooldown = {
+			frame = 0,
+			icon = 0,
+		}
+	}
 })
