289e06a1ed0d3c409612589d83cac2b345b6a379
Peter Renström <renstrom.peter@gmail.com>
2014-12-16 21:12:10 +0100

    Remove unused util function.

--- utils.lua
+++ utils.lua
@@ -1,18 +1,5 @@
 local _, addon = ...
 
--- Recursive merging of table t2 into table t1
-function addon:MergeTables(t1, t2)
-    for k, v in pairs(t2) do
-        if (type(v) == 'table') and (type(t1[k] or false) == 'table') then
-            MergeTables(t1[k], t2[k])
-        else
-            t1[k] = v
-        end
-    end
-
-    return t1
-end
-
 function addon:Round(n)
     return math.floor(n + 0.5)
 end
