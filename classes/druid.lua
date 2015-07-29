aefe7110bb26ea541507e5cf75f1be494bbe3dfb
Peter Renström <renstrom.peter@gmail.com>
2015-07-29 21:10:46 +0200

    [druid] Add Rake, Rip, Thrash and Savage Roar tracking to Feral

--- classes/druid.lua
+++ classes/druid.lua
@@ -24,3 +24,27 @@ addon:Cooldown(48505, {
     spec = 1,
     position = {'CENTER', 'UIParent', 'CENTER', -252, -42}
 })
+
+-- Rake
+addon:Debuff(155722, {
+    spec = 2,
+    position = {'CENTER', 'UIParent', 'CENTER', -210, 42}
+})
+
+-- Rip
+addon:Debuff(1079, {
+    spec = 2,
+    position = {'CENTER', 'UIParent', 'CENTER', -210, 0}
+})
+
+-- Thrash
+addon:Debuff(106830, {
+    spec = 2,
+    position = {'CENTER', 'UIParent', 'CENTER', -210, 0}
+})
+
+-- Savage Roar
+addon:Buff({52610, 174554}, {
+    spec = 2,
+    position = {'CENTER', 'UIParent', 'CENTER', -252, 42}
+})
